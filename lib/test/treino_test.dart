254e4a74d668c84fa741d2db66e12fda10e600a0
Beatriz <bestevesgoncalves@gmail.com>
2025-06-03 23:03:02 -0400

    Atualizacao dos testes 2

--- lib/test/treino_test.dart
+++ lib/test/treino_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:tcc/models/exercicio.dart';
 import 'package:tcc/models/item_treino.dart';
-import 'package:tcc/models/metodo.dart';
 import 'package:tcc/models/treino.dart';
 void main() {
   group('Treino Model', () {
